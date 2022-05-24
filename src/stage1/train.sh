68f4af9387621ca0e5c1d6274de7a197a18c62d0
xianchaow <wuxianchao@gmail.com>
2022-05-24 08:20:22 +0000

    local

--- src/stage1/train.sh
+++ src/stage1/train.sh
@@ -7,7 +7,10 @@ python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone resnet34 --period
 
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone resnet34 --period 30.0 --infer_period 30.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir resnet34 --fold 2 --seed 2023
 
+
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone resnet34 --period 30.0 --infer_period 30.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir resnet34 --fold 3 --seed 2024
+
+####
 
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone repvgg_b0 --period 30.0 --infer_period 30.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8  --logdir repvgg_b0 --fold 0 --seed 2021
 
@@ -16,6 +19,8 @@ python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone repvgg_b0 --perio
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone repvgg_b0 --period 30.0 --infer_period 30.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir repvgg_b0 --fold 2 --seed 2023
 
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone repvgg_b0 --period 30.0 --infer_period 30.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir repvgg_b0 --fold 3 --seed 2024
+
+####
 
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone efficientnet_b0 --period 20.0 --infer_period 20.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir efficientnet_b0 --fold 0 --seed 2021
 
@@ -27,6 +32,8 @@ python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone mixnet_m --period
 
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone resnest26d --period 20.0 --infer_period 20.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir resnest26d --fold 0 --seed 2020
 
+####
+
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone resnest50d_1s4x24d --period 10.0 --infer_period 10.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir resnest50d_1s4x24d --fold 1 --seed 2020
 
 python main.py --gpus 8 --batch_size 36 --epochs 50 --backbone repvgg_b2 --period 10.0 --infer_period 10.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.5 --mixup_alpha 0.8 --logdir repvgg_b2 --fold 0 --seed 2023
@@ -36,6 +43,8 @@ python main.py --gpus 8 --batch_size 36 --epochs 45 --backbone repvgg_b2 --perio
 python main.py --gpus 8 --batch_size 36 --epochs 45 --backbone resnet50d --period 10.0 --infer_period 10.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.4 --mixup_alpha 0.8 --logdir resnet50d --fold 2 --seed 4023
 
 python main.py --gpus 8 --batch_size 36 --epochs 45 --backbone resnet50d --period 10.0 --infer_period 10.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.4 --mixup_alpha 0.8 --logdir resnet50d --fold 3 --seed 4024
+
+####
 
 python main.py --gpus 8 --batch_size 36 --epochs 45 --backbone wide_resnet50_2 --period 10.0 --infer_period 10.0 --num_workers 4 --lr 1e-3 --backbone_lr 1e-4 --secondary_coef 1.0 --n_mels 128 --mixup_p 0.4 --mixup_alpha 0.8 --logdir wide_resnet50_2 --fold 2 --seed 4025
 
